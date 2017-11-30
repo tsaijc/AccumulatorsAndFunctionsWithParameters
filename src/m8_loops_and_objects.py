0239a4ea631ecc2b4d494a22d2c0915ea374a9f7
tsaijc <tsaijc@rose-hulman.edu>
2017-11-30 00:14:01 -0500

    completed to dos

--- src/m8_loops_and_objects.py
+++ src/m8_loops_and_objects.py
@@ -15,13 +15,13 @@ import math
 def main():
     """ Calls the other functions to demonstrate and/or test them. """
     # Test your functions by putting calls to them here:
-    #print_sequence1()
-    #draw_circles1()
-    #print_sequence2()
-    #draw_circles2()
-    #print_sequence3()
-    #draw_circles3()
-    #print_cosines()
+    print_sequence1()
+    draw_circles1()
+    print_sequence2()
+    draw_circles2()
+    print_sequence3()
+    draw_circles3()
+    print_cosines()
     draw_cosines_and_sines()
 
 def print_sequence1():
