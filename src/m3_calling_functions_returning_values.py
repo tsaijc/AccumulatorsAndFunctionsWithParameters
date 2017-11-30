d6ad4130e8c92438017a4245053aaf8057eb116e
tsaijc <tsaijc@rose-hulman.edu>
2017-11-29 23:09:03 -0500

    completed to dos

--- src/m3_calling_functions_returning_values.py
+++ src/m3_calling_functions_returning_values.py
@@ -31,7 +31,7 @@ def main():
     #       ** Ask a TA or your professor for help in that case. **
     # ------------------------------------------------------------------
 
-    # m3t_tester.main()
+    m3t_tester.main()
 
 
 def run_test_sum_of_digits():
@@ -87,6 +87,7 @@ def run_test_sum_of_digits():
     print('Test 4 expected:', expected3)
     print('       actual:  ', answer3)
 
+
 def sum_of_digits(number):
     """
     What comes in:  An integer.
@@ -123,7 +124,7 @@ def sum_of_digits(number):
 def run_test_digits_in_cube():
     """ Tests the   digits_in_cube   function. """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement this function.
+    # DONE: 3. Implement this function.
     #   It TESTS the  digits_in_cube  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -153,15 +154,15 @@ def run_test_digits_in_cube():
     print("Test 1 expected:", expected)
     print("       actual:  ", answer)
 
-    expected1 = 9
-    answer1 = digits_in_cube(3)
-    print('Test 2 expected:', expected1)
-    print('       actual:  ', answer1)
+    expected = 9
+    answer = digits_in_cube(3)
+    print('Test 2 expected:', expected)
+    print('       actual:  ', answer)
 
-    expected2 = 0
-    answer2 = digits_in_cube(0)
-    print('Test 3 expected:', expected2)
-    print('       actual:  ', answer2)
+    expected = 0
+    answer = digits_in_cube(0)
+    print('Test 3 expected:', expected)
+    print('       actual:  ', answer)
 
 
 def digits_in_cube(n):
@@ -174,7 +175,7 @@ def digits_in_cube(n):
       this function returns (1 + 2 + 5), which is 8.
     """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #   That is called TEST-DRIVEN DEVELOPMENT (TDD).
     #
@@ -184,11 +185,15 @@ def digits_in_cube(n):
     ####################################################################
     # ------------------------------------------------------------------
 
+    cubedi = n ** 3
+    answer = sum_of_digits(cubedi)
+    return answer
+
 
 def run_test_digits_in_power():
     """ Tests the   digits_in_power   function. """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement this function.
+    # DONE: 5. Implement this function.
     #   It TESTS the  digits_in_power  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -199,6 +204,20 @@ def run_test_digits_in_power():
     print('Testing the   digits_in_power   function:')
     print('--------------------------------------------------')
 
+    expected = 7
+    answer = digits_in_power(4,5)
+    print('Test 1 expected:', expected)
+    print('       actual:  ', answer)
+
+    expected = 27
+    answer = digits_in_power(42,3)
+    print('Test 2 expected:', expected)
+    print('       actual:  ', answer)
+
+    expected = 9
+    answer = digits_in_power(3,2)
+    print('Test 3 expected:', expected)
+    print('       actual:  ', answer)
 
 def digits_in_power(n, k):
     """
@@ -212,7 +231,7 @@ def digits_in_power(n, k):
       since 12 to the 3rd power is 1728 (whose digits sum to 18).
     """
     # ------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # DONE: 6. Implement and test this function.
     #
     ####################################################################
     # IMPORTANT: CALL, as many times as needed,
@@ -220,11 +239,15 @@ def digits_in_power(n, k):
     ####################################################################
     # ------------------------------------------------------------------
 
+    num = n ** k
+    x = sum_of_digits(num)
+    return x
+
 
 def run_test_fancy_sums_of_digits():
     """ Tests the   fancy_sums_of_digits   function. """
     # ------------------------------------------------------------------
-    # TODO: 7. Implement this function.
+    # DONE: 7. Implement this function.
     #   It TESTS the  fancy_sums_of_digits  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -236,6 +259,20 @@ def run_test_fancy_sums_of_digits():
     print('Testing the   fancy_sums_of_digits   function:')
     print('--------------------------------------------------')
 
+    expected = 1
+    answer1 = fancy_sums_of_digits(10)
+    print('Test 1 expected:', expected)
+    print('       actual:  ', answer1)
+
+    expected = 126873
+    answer2 = fancy_sums_of_digits(42)
+    print('Test 2 expected:', expected)
+    print('       actual:  ', answer2)
+
+    expected = 136075
+    answer3 = fancy_sums_of_digits(56)
+    print('Test 3 expected:', expected)
+    print('       actual:  ', answer3)
     # ------------------------------------------------------------------
     # HINT:  For your 1st test, consider  n=10.  Figure out BY HAND
     # the correct (expected) answer for that test case.  (It's easy.)
@@ -270,13 +307,18 @@ def fancy_sums_of_digits(n):
             -- so this function returns 124309.
     """
     # ------------------------------------------------------------------
-    # TODO: 8. Implement and test this function.
+    # DONE: 8. Implement and test this function.
     #
     ####################################################################
     # IMPORTANT: CALL, as many times as needed,
     #    the    sum_of_digits    function that is DEFINED ABOVE.
     ####################################################################
     # ------------------------------------------------------------------
+    x = sum_of_digits(n ** 1000)
+    y = sum_of_digits(n ** 999)
+    answer = sum_of_digits(x ** y)
+    return answer
+
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
